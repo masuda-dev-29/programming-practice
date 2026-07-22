ec08be1db7d03b621cd8ceadf41e693bc52c72a7
masuda-dev-29 <294340927+masuda- dev-29@users.noreply.github.com>
2026-07-23 01:46:42 +0900

    Lesson02: Learn about Label, Button, Entry

--- Python/household-gui-app/lesson02/main.py
+++ Python/household-gui-app/lesson02/main.py
@@ -1,10 +1,18 @@
 import tkinter as tk
 root = tk.Tk()
 root.title("Household GUI App")
-label = tk.Label(root, text="家計簿アプリ")
-button = tk.Button(root, text="追加")
 
-label.pack()
+label_title = tk.Label(root, text="家計簿アプリ")
+label_title.pack()
+
+label_amount = tk.Label(root, text="金額")
+label_amount.pack()
+
+entry = tk.Entry(root)
+entry.pack()
+
+button = tk.Button(root, text="追加")
 button.pack()
+
 
 root.mainloop()
